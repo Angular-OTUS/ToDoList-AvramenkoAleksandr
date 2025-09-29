70d4aa4e1793e9997cf9acf196459d24199bc697
Alexander Avramenko <alex.avr@gmail.com>
2025-09-29 12:41:05 +0700

    Handle text changes in the item description field

--- todoapp/src/app/components/to-do-list/to-do-list.ts
+++ todoapp/src/app/components/to-do-list/to-do-list.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, viewChild } from '@angular/core';
+
+type ToDoItem = {
+  id: string,
+  text: string
+}
 
 @Component({
   selector: 'app-to-do-list',
@@ -7,5 +12,26 @@ import { Component } from '@angular/core';
   styleUrl: './to-do-list.css'
 })
 export class ToDoList {
+  addButtonSignal = viewChild.required<ElementRef<HTMLButtonElement>>('addItemButton');
+  newItemDescriptionSignal = viewChild.required<ElementRef<HTMLInputElement>>('newItemDescriptionSignal');
 
+  items: ToDoItem[] = [];
+
+  onInputTextChange(event: Event) {
+    const inputField = event.target as HTMLInputElement;
+
+    if (inputField.id === "newItemDescription") {
+      const addButtonElement = this.addButtonSignal().nativeElement;
+      if (inputField.value?.length === 0) {
+        console.log("Disable add button");
+        addButtonElement.disabled = true;
+        addButtonElement.classList.add('button--disabled');
+      } else {
+        console.log("Enable add button");
+        addButtonElement.disabled = false;
+        addButtonElement.classList.remove('button--disabled');
+      }
+    }
+
+  }
 }
