8dfee2dfa1dccbbe34d073c8d3a55077a44ca1e4
Alexander Avramenko <alex.avr@gmail.com>
2025-12-22 12:39:46 +0700

    Fix child routes

--- todoapp/src/app/app.routes.ts
+++ todoapp/src/app/app.routes.ts
@@ -1,26 +1,26 @@
 import { Routes } from '@angular/router';
 import { ToDoList } from './components/to-do-list/to-do-list';
+import { ToDoItemView } from './components/to-do-item-view/to-do-item-view';
 
 export const routes: Routes = [
   {
     path: 'tasks',
     component: ToDoList,
     title: 'TO DO List',
-    pathMatch: 'full'
-  },
-  {
-    path: 'tasks/:id',
-    component: ToDoList,
-    title: 'TO DO List',
-    pathMatch: 'full'
+    children: [
+      {
+        path: ':id',
+        component: ToDoItemView,
+      },
+    ],
   },
   {
     path: '',
     redirectTo: '/tasks',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: '**',
-    redirectTo: '/tasks'
-  }
+    redirectTo: '/tasks',
+  },
 ];
